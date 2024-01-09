0ce0c2a2401f1047edd1e6dca8b2c527e8a4e911
Brianna Grissom <114755509+briannagrissom@users.noreply.github.com>
2024-01-09 13:05:25 -0800

    Update download-allrecipes-recipe-to-txt-file.py

--- download-allrecipes-recipe-to-txt-file.py
+++ download-allrecipes-recipe-to-txt-file.py
@@ -5,18 +5,22 @@ from scrapy import Selector
 link= input('Allrecipes Link:' )
 name= input('Item you are making: ')
 
-
+# Get the HTML
 html= requests.get(f'{link}').text
 
+# Initiate selector
 selector= Selector(text=html)
 
+# Extract the heading
 heading= selector.xpath('//h2[@id="mntl-structured-ingredients__heading_1-0"]//text()').extract_first()
 
+# Extract the ingredients
 ingredients= selector.xpath('//p/span//text()').extract()
 
-
+# Extract the second heading
 heading2= selector.xpath('//h2[@class="comp recipe__steps-heading mntl-text-block"]/text()').extract_first() # directions
 
+# Append the steps of the recipe to a list
 n= 1
 steps= []
 for i in range(20):
@@ -25,6 +29,7 @@ for i in range(20):
         steps.append(directions[0])
     n= n + 1
 
+# Initiate new txt file and write the headings, ingredients, and the steps to it
 with open(f'{name} Recipe','w') as file:
     file.write(f'{heading}\n')
     file.write('\n')
